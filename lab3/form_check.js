fb80e38984cd07863187c1fd7a5b1f5b239c6cea
bckamil <basiukajckamil@gmail.com>
2020-04-04 14:32:29 +0200

    lab3

---
+++ lab3/form_check.js
@@ -0,0 +1,96 @@
+function isWhiteSpaceOrEmpty(str) {
+    return /^[\s\t\r\n]*$/.test(str);
+}
+
+function isEmailInvalid(str) {
+    let email = /^[a-zA-Z_0-9\.]+@[a-zA-Z_0-9\.]+\.[a-zA-Z][a-zA-Z]+$/;
+    return !email.test(str);
+}
+
+function checkStringAndFocus(obj, msg, validFunc) {
+    let str = obj.value;
+    let errorFieldName = "e_" + obj.name.substr(2, obj.name.length);
+    if (validFunc(str)) {
+        document.getElementById(errorFieldName).innerHTML = msg;
+        obj.focus();
+        return false;
+    }
+    else {
+        document.getElementById(errorFieldName).innerHTML = ''
+        return true;
+    }
+}
+
+function validate(formularz) {
+    let flag = true;
+
+    if (!checkStringAndFocus(formularz.elements["f_imie"], "Podaj imię!", isWhiteSpaceOrEmpty)) {
+        flag = false;
+    }
+    if (!checkStringAndFocus(formularz.elements["f_nazwisko"], "Podaj nazwisko!", isWhiteSpaceOrEmpty)) {
+        flag = false;
+    }
+    if (!checkStringAndFocus(formularz.elements["f_kod"], "Podaj kod!", isWhiteSpaceOrEmpty)) {
+        flag = false;
+    }
+    if (!checkStringAndFocus(formularz.elements["f_ulica"], "Podaj ulicę!", isWhiteSpaceOrEmpty)) {
+        flag = false;
+    }
+    if (!checkStringAndFocus(formularz.elements["f_miasto"], "Podaj miasto!", isWhiteSpaceOrEmpty)) {
+        flag = false;
+    }
+    if (!checkStringAndFocus(formularz.elements["f_email"], "Wprowadź poprawny email!", isEmailInvalid)) {
+        flag = false;
+    }
+
+    return false;
+}
+
+function showElement(id) {
+    document.getElementById(id).style.visibility = "visible";
+}
+
+function hideElement(id) {
+    document.getElementById(id).style.visibility = "hidden";
+}
+
+function alterRows(i, e) {
+    if (e) {
+        if (i % 2 == 1) {
+            e.setAttribute("style", "background-color: Aqua;");
+        }
+        e = e.nextSibling;
+        while (e && e.nodeType != 1) {
+            e = e.nextSibling;
+        }
+        alterRows(++i, e);
+    }
+}
+
+function nextNode(e) {
+    while (e && e.nodeType != 1) {
+        e = e.nextSibling;
+    }
+    return e;
+}
+function prevNode(e) {
+    while (e && e.nodeType != 1) {
+        e = e.previousSibling;
+    }
+    return e;
+}
+function swapRows(b) {
+    let tab = prevNode(b.previousSibling);
+    let tBody = nextNode(tab.firstChild);
+    let lastNode = prevNode(tBody.lastChild);
+    tBody.removeChild(lastNode);
+    let firstNode = nextNode(tBody.firstChild);
+    tBody.insertBefore(lastNode, firstNode);
+}
+
+function cnt(form, msg, maxSize) {
+    if (form.value.length > maxSize)
+        form.value = form.value.substring(0, maxSize);
+    else
+        msg.innerHTML = maxSize - form.value.length;
+}
